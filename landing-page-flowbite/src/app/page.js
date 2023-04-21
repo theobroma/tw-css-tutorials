844ca6afcd7a7f5d645a893e5b442a049905f7bd
Alexandr Siryj <theobroma222@gmail.com>
2023-04-21 21:32:51 +0300

    add prettier

--- landing-page-flowbite/src/app/page.js
+++ landing-page-flowbite/src/app/page.js
@@ -1,19 +1,17 @@
-import Head from "next/head";
-import PageList from "../components/PageList";
-import Seaction1 from "../components/Seaction-1";
+import Head from 'next/head';
+import Seaction1 from '../components/Seaction-1';
 
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>My Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <h1 className="text-2xl font-bold">List of Pages</h1>
-                <PageList />
-                <Seaction1 />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>My Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-2xl font-bold">List of Pages</h1>
+        <Seaction1 />
+      </main>
+    </div>
+  );
 }
--- landing-page-flowbite/src/components/PageList.js
+++ landing-page-flowbite/src/components/PageList.js
@@ -1,45 +1,45 @@
 // https://stackoverflow.com/questions/74965849/youre-importing-a-component-that-needs-usestate-it-only-works-in-a-client-comp
-"use client"; // this is a client component 👈🏽
-import { useState, useEffect } from "react";
+'use client'; // this is a client component 👈🏽
+import { useState, useEffect } from 'react';
 
 const MOCK_DATA = [
-    { id: 1, title: "Page 1" },
-    { id: 2, title: "Page 2" },
-    { id: 3, title: "Page 3" },
+  { id: 1, title: 'Page 1' },
+  { id: 2, title: 'Page 2' },
+  { id: 3, title: 'Page 3' },
 ];
 
 const PageList = () => {
-    const [pages, setPages] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [pages, setPages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetchPages();
-    }, []);
+  useEffect(() => {
+    fetchPages();
+  }, []);
 
-    const fetchPages = async () => {
-        // mock api call to and to show spinner for two seconds
-        const data = await new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(MOCK_DATA);
-            }, 2000);
-        });
-        setPages(data);
-        setLoading(false);
-    };
+  const fetchPages = async () => {
+    // mock api call to and to show spinner for two seconds
+    const data = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(MOCK_DATA);
+      }, 2000);
+    });
+    setPages(data);
+    setLoading(false);
+  };
 
-    return (
-        <div>
-            {loading ? (
-                <div className="loading-indicator"></div>
-            ) : (
-                <ul>
-                    {pages.map((page) => (
-                        <li key={page.id}>{page.title}</li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {loading ? (
+        <div className="loading-indicator"></div>
+      ) : (
+        <ul>
+          {pages.map((page) => (
+            <li key={page.id}>{page.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default PageList;
