e8f7f07482d7f3b6b8c0265a2b77469b7d4c47ed
Alexandr Siryj <theobroma222@gmail.com>
2023-04-24 00:05:03 +0300

    section 2

--- landing-page-flowbite/src/app/page.js
+++ landing-page-flowbite/src/app/page.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import Seaction1 from '../components/Seaction-1';
+import Seaction1 from '@/components/Seaction-1';
+import Seaction2 from '@/components/Seaction-2';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <main>
         <h1 className="text-2xl font-bold">List of Pages</h1>
         <Seaction1 />
+        <Seaction2 />
       </main>
     </div>
   );
