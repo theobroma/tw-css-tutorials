42d8bae004b05c1d3619f18cdbc239b993ea3c3f
Alexandr Siryj <theobroma222@gmail.com>
2023-04-30 01:57:21 +0300

    section 3

--- landing-page-flowbite/src/app/page.js
+++ landing-page-flowbite/src/app/page.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Seaction1 from '@/components/Seaction-1';
 import Seaction2 from '@/components/Seaction-2';
+import Seaction3 from '@/components/Seaction-3';
 import Seaction4 from '@/components/Seaction-4';
 import Seaction5 from '@/components/Seaction-5';
 import Seaction6 from '@/components/Seaction-6';
@@ -17,6 +18,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold">List of Pages</h1>
         <Seaction1 />
         <Seaction2 />
+        <Seaction3 />
         <Seaction4 />
         <Seaction5 />
         <Seaction6 />
